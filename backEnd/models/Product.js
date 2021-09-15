52ed284c50efcec58b8c6c5c68392dbb3325b2ae
Melek Lassoued <65103260+meleklassoued@users.noreply.github.com>
2021-09-15 17:26:42 +0100

    fix the model

--- backEnd/models/Product.js
+++ backEnd/models/Product.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
-const productSchema = new mongoose.Schema({
-  product: {
+const productSchema = mongoose.Schema({
+  name: {
     type: String,
     required: true,
   },
@@ -23,6 +23,6 @@ const productSchema = new mongoose.Schema({
   },
 });
 
-const productModel = mongoose.model("Product", productSchema);
+const Product = mongoose.model("product", productSchema);
 
-module.export = productModel;
+module.exports = Product;
