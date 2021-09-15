c88865357782a6842fc7f74b9210e5952a79997c
Melek Lassoued <65103260+meleklassoued@users.noreply.github.com>
2021-09-15 16:24:34 +0100

    create a modelProduct with mongoose

---
+++ backEnd/models/Product.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+
+const productSchema = new mongoose.Schema({
+  product: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  countInStock: {
+    type: Number,
+    required: true,
+  },
+  imageUrl: {
+    type: String,
+    required: true,
+  },
+});
+
+const productModel = mongoose.model("Product", productSchema);
+
+module.export = productModel;
