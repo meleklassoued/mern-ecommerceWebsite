68d4ca1d18360945ff0725e677f78baea9be8cb4
Melek Lassoued <65103260+meleklassoued@users.noreply.github.com>
2021-09-16 16:59:38 +0100

    clean Up the code and make it more better

--- backEnd/server.js
+++ backEnd/server.js
@@ -1,10 +1,32 @@
+/* -------------------------- import configuration -------------------------- */
 require("dotenv").config();
+/* -------------------------- finish configuration -------------------------- */
+
+/* -------------------------------------------------------------------------- */
+/*                              require constants                             */
+/* -------------------------------------------------------------------------- */
 const express = require("express");
 const connectDB = require("./config/db");
-connectDB();
+const productRoutes = require("./routes/productRouters");
 const app = express();
-const PORT = process.env.PORT || 5500;
+/* -------------------------------------------------------------------------- */
+/*                              Finish                            */
+/* -------------------------------------------------------------------------- */
 
+//connection to the data base
+connectDB();
+// finish connection
+app.use(express.json());
+// app.use("/products", productRoutes);
+
+
+
+
+
+
+
+//listenning to the port .
+const PORT = process.env.PORT || 5500
 app.listen(PORT, () => {
   console.log("we are on the port nikomek");
 });
