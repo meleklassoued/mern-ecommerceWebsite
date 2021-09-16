4b160f290f0884c525e905f14ee52111e49daa98
Melek Lassoued <65103260+meleklassoued@users.noreply.github.com>
2021-09-16 17:00:54 +0100

    get all : first Route

---
+++ backEnd/routes/productRouters.js
@@ -0,0 +1,11 @@
+/* -------------------------------------------------------------------------- */
+/*                              import constants                              */
+/* -------------------------------------------------------------------------- */
+const express = require("express");
+const router = express.Router();
+
+// desc get all products from DB
+// route Get/api/products/:id
+router.get("/", (req, res) => {});
+
+module.export = router;
