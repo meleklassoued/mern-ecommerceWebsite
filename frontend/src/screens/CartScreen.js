a43cf966d92c71edcafa7d4f1b7d1aac47661c5c
Melek Lassoued <65103260+meleklassoued@users.noreply.github.com>
2021-09-24 02:44:17 +0100

    finish styling cartscreen

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -1,7 +1,23 @@
 import "./Cartscreen.css";
-
+import CartItem from "../components/CartItem";
 const Cartscreen = () => {
-  return <div className='cartScreen'> cart screen</div>;
+  return (
+    <div className='cartScreen'>
+      <div className='cartscreen__left'>
+        <h2>shooping cart</h2>
+        <CartItem />
+      </div>
+      <div className='cartscreen__right'>
+        <div className='cartscreen__info'>
+          <p>subtitle (0) items</p>
+          <p>$553</p>
+        </div>
+        <div>
+          <button>Proceed To checkout</button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Cartscreen;
