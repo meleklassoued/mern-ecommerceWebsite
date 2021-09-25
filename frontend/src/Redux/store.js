8c2f50433d0e7a9451196ae766a8126163406ccd
Melek Lassoued <65103260+meleklassoued@users.noreply.github.com>
2021-09-25 18:51:44 +0100

    import reducers

--- frontend/src/Redux/store.js
+++ frontend/src/Redux/store.js
@@ -1,8 +1,17 @@
 import { createStore, combineReducers, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
 import { composeWithDevTools } from "redux-devtools-extension";
-
-const reducer = combineReducers({});
+//import reducers
+import { cartReducer } from "./reducers/cartReducers";
+import {
+  getProductsDetails,
+  getProductsReducer,
+} from "./reducers/productReducers";
+const reducer = combineReducers({
+  cart: cartReducer,
+  getProducts: getProductsReducer,
+  getDetails: getProductsDetails,
+});
 
 const middleware = [thunk];
 
