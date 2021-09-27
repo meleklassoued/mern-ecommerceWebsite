f5a80b40221495a1fc2f12c0529640f0e3c14872
Melek Lassoued <65103260+meleklassoued@users.noreply.github.com>
2021-09-27 15:18:24 +0100

    adding props to the product componnent

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -1,24 +1,16 @@
 import "./product.css";
 import { Link } from "react-router-dom";
 
-const Product = () => {
+const Product = ({ imageUrl, name, price, description, productId }) => {
   return (
     <div className='Product'>
-      <img
-        src='https://images.unsplash.com/photo-1606813907291-d86efa9b94db?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1352&q=80'
-        alt='Product  Name'
-      />
+      <img src={imageUrl} alt={name} />
       <div className='product__info'>
-        <p className='info__name'>Product1</p>
-        <p className='info__description'>
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sequi eius
-          assumenda sapiente ut fugit, culpa eum veniam adipisci. Magni nam
-          repudiandae similique laboriosam quaerat cumque saepe molestias
-          cupiditate porro. Illum!
-        </p>
-        <p className='info__price'>$332</p>
+        <p className='info__name'>{name}</p>
+        <p className='info__description'>{description.substring(0, 100)}...</p>
+        <p className='info__price'>${price}</p>
 
-        <Link to={`/product/${1111}`} className='info__button'>
+        <Link to={`/product/${productId}`} className='info__button'>
           view
         </Link>
       </div>
