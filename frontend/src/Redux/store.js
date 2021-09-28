a1d7f7b79ab946f3946c38b2bf696db07b842879
Melek Lassoued <65103260+meleklassoued@users.noreply.github.com>
2021-09-28 19:43:17 +0100

    add the Items to the local storage when werefressh

--- frontend/src/Redux/store.js
+++ frontend/src/Redux/store.js
@@ -15,8 +15,18 @@ const reducer = combineReducers({
 
 const middleware = [thunk];
 
+const CartFromLocalStorage = localStorage.getItem("cart")
+  ? JSON.parse(localStorage.getItem("cart"))
+  : [];
+
+const INITIAL_STATE = {
+  cart: {
+    cartItems: CartFromLocalStorage,
+  },
+};
 const store = createStore(
   reducer,
+  INITIAL_STATE,
   composeWithDevTools(applyMiddleware(...middleware)),
 );
 
